e6351226fa3e263f0402da36e187707d578721ff
clgleroux <clement@marketparts.com>
2024-02-13 13:07:43 +0100

    :wrench: App todolist

--- todolist/App.js
+++ todolist/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
 
@@ -6,7 +6,7 @@ import FormTodolist from "./components/FormTodolist";
 import ElmentTodolist from "./components/ElmentTodolist";
 
 export default function App() {
-  const [todolists, onChangeTodolists] = React.useState([]);
+  const [todolists, onChangeTodolists] = useState([]);
 
   const onDelete = (index) => {
     let temp = [...todolists];
